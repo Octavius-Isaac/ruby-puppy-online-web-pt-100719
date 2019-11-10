0d6d0ae7dd7f076da979985f5f2a4117ec1343df
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-11-10 00:19:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,11 +8,22 @@ class Dog
     @name = dog_name 
   end 
   
-  def self.all
-    @@all
-  end 
-  
-  
+   def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each {|dog| puts dog.name}
+    end
+
+    def save
+        @@all << self
+    end
+
   
   
 end 
